30171dac4de1693039e32e432f43167b76ac002a
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2026-04-18 15:11:48 -0500

    win32u: Track swapchain/opengl reference of client surface.

--- dlls/win32u/vulkan.c
+++ dlls/win32u/vulkan.c
@@ -145,6 +145,7 @@ struct surface
 {
     struct vulkan_surface obj;
     struct client_surface *client;
+    struct swapchain *swapchain;
     HWND hwnd;
 };
 
@@ -3128,14 +3129,18 @@ static VkResult win32u_vkCreateSwapchainKHR( VkDevice client_device, const VkSwa
                    create_info_host.imageFormat );
     }
 
+    InterlockedIncrement( &surface->client->busy_ref );
+
     if ((res = device->p_vkCreateSwapchainKHR( device->host.device, &create_info_host, NULL, &host_swapchain )))
     {
+        InterlockedDecrement( &surface->client->busy_ref );
         free( swapchain );
         return res;
     }
 
     vulkan_object_init( &swapchain->obj.obj, host_swapchain );
     swapchain->surface = surface;
+    surface->swapchain = swapchain;
     swapchain->extents = create_info->imageExtent;
     instance->p_insert_object( instance, &swapchain->obj.obj );
 
@@ -3171,6 +3176,7 @@ void win32u_vkDestroySwapchainKHR( VkDevice client_device, VkSwapchainKHR client
     struct vulkan_device *device = vulkan_device_from_handle( client_device );
     struct vulkan_instance *instance = device->physical_device->instance;
     struct swapchain *swapchain = swapchain_from_handle( client_swapchain );
+    struct surface *surface;
 
     if (allocator) FIXME( "Support for allocation callbacks not implemented yet\n" );
     if (!swapchain) return;
@@ -3198,6 +3204,11 @@ void win32u_vkDestroySwapchainKHR( VkDevice client_device, VkSwapchainKHR client
     }
 
     device->p_vkDestroySwapchainKHR( device->host.device, swapchain->obj.host.swapchain, NULL );
+    if ((surface = swapchain->surface))
+    {
+        surface->swapchain = NULL;
+        InterlockedDecrement( &surface->client->busy_ref );
+    }
     instance->p_remove_object( instance, &swapchain->obj.obj );
 
     free( swapchain );
